315d0ee23c8277e3b057b77d070c24ea7301cb2b
Toshiki Nakamura <82020865+ToshikiNakamura0412@users.noreply.github.com>
2024-05-09 01:34:21 +0900

    Update documentation (#106)

--- docs/menudata.js
+++ docs/menudata.js
@@ -60,7 +60,6 @@ var menudata={children:[
 {text:"n",url:"functions_func.html#index_n"},
 {text:"o",url:"functions_func.html#index_o"},
 {text:"p",url:"functions_func.html#index_p"},
-{text:"r",url:"functions_func.html#index_r"},
 {text:"s",url:"functions_func.html#index_s"},
 {text:"t",url:"functions_func.html#index_t"},
 {text:"v",url:"functions_func.html#index_v"},
